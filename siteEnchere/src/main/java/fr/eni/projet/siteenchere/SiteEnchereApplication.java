3e0b56fdcbc362c4cd606e8bdd8555ca1cf7d39a
yeyemelodie01 <melodieyeye@gmail.com>
2024-02-08 10:00:37 +0100

    add files

---
+++ siteEnchere/src/main/java/fr/eni/projet/siteenchere/SiteEnchereApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.projet.siteenchere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteEnchereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiteEnchereApplication.class, args);
+    }
+
+}
---
+++ siteEnchere/src/test/java/fr/eni/projet/siteenchere/SiteEnchereApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.projet.siteenchere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteEnchereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
