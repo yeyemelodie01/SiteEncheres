45f23134ed28264bc8a7fd6180d04ee418c5d1f5
yeyemelodie01 <melodieyeye@gmail.com>
2024-02-09 16:15:51 +0100

    add text in UserDAO

--- siteEnchere/src/main/java/fr/eni/projet/siteenchere/dal/UserDAO.java
+++ siteEnchere/src/main/java/fr/eni/projet/siteenchere/dal/UserDAO.java
@@ -1,4 +1,10 @@
 package fr.eni.projet.siteenchere.dal;
 
+import fr.eni.projet.siteenchere.bo.User;
+
 public interface UserDAO {
+    Long createUser(User user);
+    User readUserById(Long user);
+    User readUserByEmail(String email);
+
 }
